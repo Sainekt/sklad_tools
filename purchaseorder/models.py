44be578e2807d7d4ba4d7b1b7a9e19731a43b66e
Sainekt <xcision@ya.ru>
2024-08-05 17:20:19 +0300

    add product models

--- purchaseorder/models.py
+++ purchaseorder/models.py
@@ -7,15 +7,31 @@ class Order(models.Model):
     created_at = models.DateField('Дата создания', auto_now_add=True)
 
 
+class Product(models.Model):
+    product_id = models.SlugField(max_length=100, verbose_name='ID товара')
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    code = models.CharField(
+        max_length=100, verbose_name='Код товара', null=True, blank=True
+    )
+    article = models.CharField(
+        max_length=100, verbose_name='Код товара', null=True, blank=True
+    )
+    barcodes = models.CharField(
+        max_length=250, blank=True, null=True, verbose_name='Штрих-коды'
+    )
+    cell_number = models.CharField(
+        verbose_name='Номер ячейки', max_length=100, null=True, blank=True
+    )
+    image = models.ImageField(
+        verbose_name='Фото', blank=True, upload_to='purchaseorder', null=True)
+
+
 class PurchaseOrder(models.Model):
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name='products')
-    image = models.ImageField(
-        verbose_name='Фото', blank=True, upload_to='purchaseorder', null=True)
-    name = models.CharField('Название товара', max_length=250)
-    code = models.CharField('Код товара', max_length=100)
-    barcodes = models.CharField(
-        'Баркоды', null=True, blank=True, max_length=250)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='products'
+    )
     quantity = models.IntegerField('Заказано шт')
     summ = models.FloatField('Сумма')
     fact = models.IntegerField('Фактическое количество', default=0)
