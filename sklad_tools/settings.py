2807f92f226f149ad1057f70200e7ecc68d4c8f9
Sainekt <xcision@ya.ru>
2024-08-13 16:50:14 +0300

    del not used code

--- sklad_tools/settings.py
+++ sklad_tools/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'sklad_tools.urls'
